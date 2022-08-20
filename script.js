3a521780a44434c0b869e6027d410e707f304af9
maria <104516362+maria1254@users.noreply.github.com>
2022-08-21 01:42:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,715 @@
+
+function mySubmit(){
+    var val1 = document.frmMain.sct1.value;
+    var val2 = document.frmMain.sct2.value;
+    var myURL;
+    
+    switch(val1+val2){
+    
+    
+    case "1A":
+    myURL = "1a-b.mp4"
+    break;
+
+
+
+    case "1B":
+        alert("目的地は現在地と同じ1階A階段カテゴリーです。\nページを戻り、先ほどのマップから目的地をご確認ください。");
+        window.location.href = "";
+    break;
+    
+    case "1C":
+    
+    break;
+    
+    case "1D":
+        alert("目的地は現在地と同じ1階A階段カテゴリーです。\nページを戻り、先ほどのマップから目的地をご確認ください。");
+        window.location.href = "";
+    break;
+    
+    
+    case "1E":
+        alert("目的地は現在地と同じ1階A階段カテゴリーです。\nページを戻り、先ほどのマップから目的地をご確認ください。");
+        window.location.href = "";
+    break;
+    
+    
+    case "1F":
+        alert("目的地は現在地と同じ1階A階段カテゴリーです。\nページを戻り、先ほどのマップから目的地をご確認ください。");
+        window.location.href = "";
+    break;
+    
+    
+    case "1G":
+        alert("目的地は現在地と同じ1階A階段カテゴリーです。\nページを戻り、先ほどのマップから目的地をご確認ください。");
+        window.location.href = "";
+    break;
+    
+    
+    case "1H":
+        alert("目的地は現在地と同じ1階A階段カテゴリーです。\nページを戻り、先ほどのマップから目的地をご確認ください。");
+        window.location.href = "";
+    break;
+    
+    
+    case "1I":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1J":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1K":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1L":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1M":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1N":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1O":
+    myURL = "1a-d.mp4"
+    break;
+    
+    
+    case "1P":
+    myURL = "1a-d.mp4"
+    break;
+    
+    
+    case "1Q":
+    myURL = ""
+    break;
+    
+    
+    case "1R":
+    myURL = ""
+    break;
+    
+    
+    case "1S":
+    myURL = "1a-d.mp4"
+    break;
+    
+    
+    case "1T":
+    myURL = "1a-c.mp4"
+    break;
+    
+    
+    case "1U":
+    myURL = "1a-b.mp4"
+    break;
+    
+    
+    case "1V":
+    myURL = "1a-b.mp4"
+    break;
+    
+
+
+
+
+
+
+
+
+    
+    case "1W":
+    myURL = ""
+    break;
+    
+    
+    
+    case "1X":
+    myURL = ""
+    break;
+    
+    
+    case "1Y":
+    myURL = ""
+    break;
+    
+    
+    case "1Z":
+    myURL = ""
+    break;
+    
+    
+    
+    
+    case "1AA":
+    myURL = ""
+    break;
+    
+    case "1BB":
+    myURL = ""
+    break;
+    
+    case "1CC":
+    myURL = ""
+    break;
+    
+    case "1DD":
+    myURL = ""
+    break;
+    
+    
+    case "1EE":
+    myURL = ""
+    break;
+    
+    
+    case "1FF":
+    myURL = ""
+    break;
+    
+    
+    case "1GG":
+    myURL = ""
+    break;
+    
+    
+    case "1HH":
+    myURL = ""
+    break;
+    
+    
+    case "1II":
+    myURL = ""
+    break;
+    
+    
+    case "1JJ":
+    myURL = ""
+    break;
+    
+    
+    case "1KK":
+    myURL = ""
+    break;
+    
+    
+    case "1LL":
+    myURL = ""
+    break;
+    
+    
+    case "1MM":
+    myURL = ""
+    break;
+    
+    
+    case "1NN":
+    myURL = ""
+    break;
+    
+    
+    case "1OO":
+    myURL = ""
+    break;
+    
+    
+    case "1PP":
+    myURL = ""
+    break;
+    
+    
+    case "1QQ":
+    myURL = ""
+    break;
+    
+    
+    case "1RR":
+    myURL = ""
+    break;
+    
+    
+    case "1SS":
+    myURL = ""
+    break;
+    
+    
+    case "1TT":
+    myURL = ""
+    break;
+    
+    
+    case "1UU":
+    myURL = ""
+    break;
+    
+    
+    case "1VV":
+    myURL = ""
+    break;
+    
+    
+    case "1WW":
+    myURL = ""
+    break;
+    
+    
+    case "1XX":
+    myURL = ""
+    break;
+    
+    
+    case "1YY":
+    myURL = ""
+    break;
+    
+    
+    case "1ZZ":
+    myURL = ""
+    break;
+    
+    
+    case "1a":
+    myURL = ""
+    break;
+    
+    case "1b":
+    myURL = ""
+    break;
+    
+    
+    case "1c":
+    myURL = ""
+    break;
+    
+    case "1d":
+    myURL = ""
+    break;
+    
+    case "1e":
+    myURL = ""
+    break;
+    
+    case "1f":
+    myURL = ""
+    break;
+    
+    
+    case "1g":
+    myURL = ""
+    break;
+    
+    case "1h":
+    myURL = ""
+    break;
+    
+    case "1i":
+    myURL = ""
+    break;
+    
+    case "1j":
+    myURL = ""
+    break;
+    
+    case "1k":
+    myURL = ""
+    break;
+    
+    case "1l":
+    myURL = ""
+    break;
+    
+    case "1m":
+    myURL = ""
+    break;
+    
+    case "1n":
+    myURL = ""
+    break;
+    
+    case "1o":
+    myURL = ""
+    break;
+    
+    case "1p":
+    myURL = ""
+    break;
+    
+    case "1q":
+    myURL = ""
+    break;
+    
+    case "1r":
+    myURL = ""
+    break;
+    
+    case "1s":
+    myURL = ""
+    break;
+    
+    case "1t":
+    myURL = ""
+    break;
+    
+    _____________________________________________
+    
+    
+    
+        
+    case "A":
+    myURL = ""
+    break;
+    
+    case "B":
+    myURL = ""
+    break;
+    
+    case "C":
+    myURL = ""
+    break;
+    
+    case "D":
+    myURL = ""
+    break;
+    
+    
+    case "E":
+    myURL = ""
+    break;
+    
+    
+    case "F":
+    myURL = ""
+    break;
+    
+    
+    case "G":
+    myURL = ""
+    break;
+    
+    
+    case "H":
+    myURL = ""
+    break;
+    
+    
+    case "I":
+    myURL = ""
+    break;
+    
+    
+    case "J":
+    myURL = ""
+    break;
+    
+    
+    case "K":
+    myURL = ""
+    break;
+    
+    
+    case "L":
+    myURL = ""
+    break;
+    
+    
+    case "M":
+    myURL = ""
+    break;
+    
+    
+    case "N":
+    myURL = ""
+    break;
+    
+    
+    case "O":
+    myURL = ""
+    break;
+    
+    
+    case "P":
+    myURL = ""
+    break;
+    
+    
+    case "Q":
+    myURL = ""
+    break;
+    
+    
+    case "R":
+    myURL = ""
+    break;
+    
+    
+    case "S":
+    myURL = ""
+    break;
+    
+    
+    case "T":
+    myURL = ""
+    break;
+    
+    
+    case "U":
+    myURL = ""
+    break;
+    
+    
+    case "V":
+    myURL = ""
+    break;
+    
+    
+    case "W":
+    myURL = ""
+    break;
+    
+    
+    case "X":
+    myURL = ""
+    break;
+    
+    
+    case "Y":
+    myURL = ""
+    break;
+    
+    
+    case "Z":
+    myURL = ""
+    break;
+    
+    
+    
+    
+    case "AA":
+    myURL = ""
+    break;
+    
+    case "BB":
+    myURL = ""
+    break;
+    
+    case "CC":
+    myURL = ""
+    break;
+    
+    case "DD":
+    myURL = ""
+    break;
+    
+    
+    case "EE":
+    myURL = ""
+    break;
+    
+    
+    case "FF":
+    myURL = ""
+    break;
+    
+    
+    case "GG":
+    myURL = ""
+    break;
+    
+    
+    case "HH":
+    myURL = ""
+    break;
+    
+    
+    case "II":
+    myURL = ""
+    break;
+    
+    
+    case "JJ":
+    myURL = ""
+    break;
+    
+    
+    case "KK":
+    myURL = ""
+    break;
+    
+    
+    case "LL":
+    myURL = ""
+    break;
+    
+    
+    case "MM":
+    myURL = ""
+    break;
+    
+    
+    case "NN":
+    myURL = ""
+    break;
+    
+    
+    case "OO":
+    myURL = ""
+    break;
+    
+    
+    case "PP":
+    myURL = ""
+    break;
+    
+    
+    case "QQ":
+    myURL = ""
+    break;
+    
+    
+    case "RR":
+    myURL = ""
+    break;
+    
+    
+    case "SS":
+    myURL = ""
+    break;
+    
+    
+    case "TT":
+    myURL = ""
+    break;
+    
+    
+    case "UU":
+    myURL = ""
+    break;
+    
+    
+    case "VV":
+    myURL = ""
+    break;
+    
+    
+    case "WW":
+    myURL = ""
+    break;
+    
+    
+    case "XX":
+    myURL = ""
+    break;
+    
+    
+    case "YY":
+    myURL = ""
+    break;
+    
+    
+    case "ZZ":
+    myURL = ""
+    break;
+    
+    
+    case "a":
+    myURL = ""
+    break;
+    
+    case "b":
+    myURL = ""
+    break;
+    
+    
+    case "c":
+    myURL = ""
+    break;
+    
+    case "d":
+    myURL = ""
+    break;
+    
+    case "e":
+    myURL = ""
+    break;
+    
+    case "f":
+    myURL = ""
+    break;
+    
+    
+    case "g":
+    myURL = ""
+    break;
+    
+    case "h":
+    myURL = ""
+    break;
+    
+    case "i":
+    myURL = ""
+    break;
+    
+    case "j":
+    myURL = ""
+    break;
+    
+    case "k":
+    myURL = ""
+    break;
+    
+    case "l":
+    myURL = ""
+    break;
+    
+    case "m":
+    myURL = ""
+    break;
+    
+    case "n":
+    myURL = ""
+    break;
+    
+    case "o":
+    myURL = ""
+    break;
+    
+    case "p":
+    myURL = ""
+    break;
+    
+    case "q":
+    myURL = ""
+    break;
+    
+    case "r":
+    myURL = ""
+    break;
+    
+    case "s":
+    myURL = ""
+    break;
+    
+    case "t":
+    myURL = ""
+    break;
+    
+    
+    
+    
+    }
+    
+    location.href = myURL;
+    return 0;
+    }
